0e599075e1571099561618d3f9961c08b37df6d5
Sike Osinuga <adesikeoye@yahoo.com>
2020-02-13 21:44:28 +0000

    completed programming fundamentals 003

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -9,3 +9,50 @@ describe("catalogueService", () => {
     });
   });
 });
+describe("catalogueService.checkBookByTitle", () => {
+  test("returns true if the book exists", () => {
+    expect(
+      catalogueService.checkBookByTitle(
+        "The Assassination of Margaret Thatcher"
+      )
+    ).toBe(true);
+  });
+});
+test("returns false if the book doesn't exist", () => {
+  expect(
+    catalogueService.checkBookByTitle(
+      "The Asassination of Margaret Thatcher"
+    )
+  ).toBe(false);
+});
+describe("catalogueService.countBooksByFirstLetter", () => {
+  test("returns the total number pf books that begin with the given letter", () => {
+    expect(
+      catalogueService.countBooksByFirstLetter("W")).toBe(2);
+  });
+});
+test("returns the total number of books that begin with the given letter", () => {
+  expect(
+    catalogueService.countBooksByFirstLetter("w")).toBe(2);
+});
+describe("catalogueService.getQuantity", () => {
+  test("returns the total number of a given title", () => {
+    expect(
+      catalogueService.getQuantity("A Place of Greater Safety")).toBe(11);
+  });
+});
+describe("catalogueService.getBooksByAuthor", () => {
+  test("returns the total number of books by a given title", () => {
+    expect(
+      catalogueService.getBooksByAuthor("Robert Bolaño")).toEqual([
+        { title: "2666", author: "Robert Bolaño", quantity: 17 },
+        { title: "By Night In Chile", author: "Robert Bolaño", quantity: 8 }
+      ]);
+  });
+});
+describe("catalogueService.checkQuantity", () => {
+  test("returns true if there are at least as many books in stock as the given quantity", () => {
+    expect(
+      catalogueService.checkQuantity("By Night In Chile", 4)).toBe(true);
+  });
+});
